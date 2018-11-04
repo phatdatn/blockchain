34788e978d116cc20f7f0442c6061dbe664fd63c
Phat Dat Nguyen <phatdatn@berkeley.edu>
2018-11-04 03:04:06 -0800

    Updating ethereum dapp

--- votedapp/contracts/Voting.sol
+++ votedapp/contracts/Voting.sol
@@ -6,7 +6,32 @@ contract Voting {
 
   /* Constructor */
     constructor () public {
-      candidate = "Person 1";
+      addCandidate("Sinjon Santos");
+      addCandidate("Phat Dat Nguyen");
 
     }
+
+  /* Modeling a Candidate */
+  struct Candidate {
+    uint id;
+    uint votecount;
+    string name;
+  }
+
+/* Stores Candidates, interacts with data layer of blockchain
+  Key is unsigned integer, value is
+  candidate structure type */
+  mapping(uint => Candidate) public candidates;
+
+  /* Store Candidate count */
+  uint public candidatesCount;
+
+  /* Adds a Candidate to MAPPING with COUNT, _NAME, and 0 votes*/
+  function addCandidate (string _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, 0, _name);
+  }
+
+
+
 }
