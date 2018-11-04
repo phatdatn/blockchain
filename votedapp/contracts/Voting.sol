bec5220871ce7b8a0e7e2feb293e60779c2bf690
Phat Dat Nguyen <phatdatn@berkeley.edu>
2018-11-04 01:11:54 -0700

    Added ether dapp

---
+++ votedapp/contracts/Voting.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+contract Voting {
+  /* State variable candidate */
+  string public candidate;
+
+  /* Constructor */
+    constructor () public {
+      candidate = "Person 1";
+
+    }
+}
